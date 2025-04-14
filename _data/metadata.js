da483dcf1560669c2eb80e8309b1c38d79d62b36
Robin Berjon <robin.berjon@nytimes.com>
2025-04-14 16:30:14 +0200

    very basics

---
+++ _data/metadata.js
@@ -0,0 +1,12 @@
+
+export default {
+	title: "Infrastructuring",
+	url: "https://infrastructur.ing/",
+	language: "en",
+	description: "Digital infrastructure for a democratic future",
+	author: {
+		name: "Robin Berjon",
+		email: "robin@berjon.com",
+		url: "https://berjon.com/about/",
+	},
+};
---
+++ content/projects/indiesky.md
@@ -0,0 +1,12 @@
+---
+layout: layouts/project.njk
+title: "Indiesky"
+maturity: outline
+description:
+  "tk"
+tags:
+  - project
+  - docs
+---
+
+Tk
